10e32d84d8b19da6ad0315b5a93a2f7191482ce8
divya jain <mail.divya.jain@gmail.com>
2017-08-28 21:07:09 +0530

    Introduced Car class to ParkingLot. Failing test case

--- src/main/java/model/ParkingLot.java
+++ src/main/java/model/ParkingLot.java
@@ -1,9 +1,16 @@
 package model;
 
+import java.util.HashMap;
+
 public class ParkingLot {
 
     private int Totalslots;
+    HashMap<Integer,Car> parkinglotmapping = new HashMap<Integer, Car>();
     public ParkingLot(int slots ) {
         this.Totalslots=slots;
+
+        for(int i=1;i<=slots;i++){
+            parkinglotmapping.put(i,null);
+        }
     }
 }
--- src/test/java/ParkingLotTesting.java
+++ src/test/java/ParkingLotTesting.java
@@ -5,8 +5,13 @@ import static org.junit.Assert.*;
 public class ParkingLotTesting {
     @Test
     public void testParkingLotClass(){
-
         ParkingLot parkinglot = new ParkingLot(4);
         assertEquals(ParkingLot.class,parkinglot.getClass());
+    }
+
+    @Test
+    public void testCarClass(){
+        Car car = new Car();
+        assertEquals(Car.class,car.getClass());
     }
 }
